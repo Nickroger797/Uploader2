1ac89242ef8ae8a4a20230c75506165997370bc0
Kamlesh Saini <88968064+oVo-HxBots@users.noreply.github.com>
2024-02-24 19:40:59 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -1,22 +1,20 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
-from pyrogram import Client as Tech_VJ
+
+from pyrogram import Client
 from pyrogram import filters, enums
 from config import Config
-from database.access import techvj
+from database.access import DB
 from plugins.buttons import *
 
-@Tech_VJ.on_message(filters.private & filters.command('total'))
+@Client.on_message(filters.private & filters.command('total'))
 async def sts(c, m):
-    if m.from_user.id != Config.TECH_VJ_OWNER_ID:
+    if m.from_user.id != Config.OWNER_ID:
         return 
-    total_users = await techvj.total_users_count()
+    total_users = await DB.total_users_count()
     await m.reply_text(text=f"Total user(s) {total_users}", quote=True)
 
 
-@Tech_VJ.on_message(filters.private & filters.command("search"))
+@Client.on_message(filters.private & filters.command("search"))
 async def serc(bot, update):
 
       await bot.send_message(chat_id=update.chat.id, text="🔍 TORRENT SEARCH", 
