cc389b42aee8b1fe62029debff1ccef70d2d418a
Kamlesh Saini <88968064+oVo-HxBots@users.noreply.github.com>
2024-02-24 19:31:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
 import os
 import re
@@ -17,57 +14,52 @@ def is_enabled(value, default):
 
 class Config(object):
     # Bot Information 
-    TECH_VJ_BOT_TOKEN = os.environ.get("TECH_VJ_BOT_TOKEN", "")
-    TECH_VJ_BOT_USERNAME = os.environ.get("TECH_VJ_BOT_USERNAME", "") # Bot username without @.
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "") # Bot username without @.
     
     # The Telegram API things
-    TECH_VJ_API_ID = int(os.environ.get("TECH_VJ_API_ID", ""))
-    TECH_VJ_API_HASH = os.environ.get("TECH_VJ_API_HASH", "")
+    API_ID = int(os.environ.get("API_ID", ""))
+    API_HASH = os.environ.get("API_HASH", "")
     
     # the download location, where the HTTP Server runs
-    TECH_VJ_DOWNLOAD_LOCATION = "./DOWNLOADS"
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
     
     # Telegram maximum file upload size
-    TECH_VJ_MAX_FILE_SIZE = 50000000
-    TECH_VJ_TG_MAX_FILE_SIZE = 4194304000 #2097152000
-    TECH_VJ_FREE_USER_MAX_FILE_SIZE = 50000000
+    MAX_FILE_SIZE = 50000000
+    TG_MAX_FILE_SIZE = 4194304000 #2097152000
+    FREE_USER_MAX_FILE_SIZE = 50000000
     
     # chunk size that should be used with requests
-    TECH_VJ_CHUNK_SIZE = int(128)
+    CHUNK_SIZE = int(128)
     # default thumbnail to be used in the videos
     
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
-    TECH_VJ_HTTP_PROXY = ""
+    HTTP_PROXY = ""
     
     # maximum message length in Telegram
-    TECH_VJ_MAX_MESSAGE_LENGTH = 4096
+    MAX_MESSAGE_LENGTH = 4096
     
     # set timeout for subprocess
-    TECH_VJ_PROCESS_MAX_TIMEOUT = 3600
+    PROCESS_MAX_TIMEOUT = 3600
     
     # your telegram account id
-    TECH_VJ_OWNER_ID = int(os.environ.get("TECH_VJ_OWNER_ID", "")) 
-    TECH_VJ_SESSION_NAME = "VJ-URL-UPLOADER-BOT"
+    OWNER_ID = int(os.environ.get("OWNER_ID", "")) 
+    SESSION_NAME = "Hx_URLuploadBot"
     
     # database uri (mongodb)
-    TECH_VJ_DATABASE_URL = os.environ.get("TECH_VJ_DATABASE_URL", "")
-    TECH_VJ_MAX_RESULTS = "50"
+    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    MAX_RESULTS = "50"
 
     # channel information
-    TECH_VJ_LOG_CHANNEL = int(os.environ.get("TECH_VJ_LOG_CHANNEL", "")) # your log channel id and make bot admin in log channel with full right 
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "")) # your log channel id and make bot admin in log channel with full right 
     
     # if you want force subscribe then give your channel id below else leave blank
-    tech_vj_update_channel = environ.get('TECH_VJ_UPDATES_CHANNEL', '') # your update channel id and make bot admin in update channel with full right
-    TECH_VJ_UPDATES_CHANNEL = int(tech_vj_update_channel) if tech_vj_update_channel and id_pattern.search(tech_vj_update_channel) else None  
+    update_channel = environ.get('UPDATES_CHANNEL', '') # your update channel id and make bot admin in update channel with full right
+    UPDATES_CHANNEL = int(update_channel) if update_channel and id_pattern.search(update_channel) else None  
     
     # Url Shortner Information 
-    TECH_VJ = bool(environ.get('TECH_VJ', True)) # Set False If you want shortlink off else True
-    TECH_VJ_URL = environ.get('TECH_VJ_URL', 'moneykamalo.com') # your shortlink url domain or url without https://
-    TECH_VJ_API = environ.get('TECH_VJ_API', '0eefb93e1e3ce9470a7033115ceb1bad13a9d674') # your url shortner api
-    TECH_VJ_TUTORIAL = os.environ.get("TECH_VJ_TUTORIAL", "https://t.me/How_To_Open_Linkl")
-
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+    SHORTNER = bool(environ.get('SHORTNER', True)) # Set False If you want shortlink off else True
+    SHORTNER_URL = environ.get('URL', 'linkrex.net') # your shortlink url domain or url without https://
+    SHORTNER_API = environ.get('API', 'bcf8fc3fd10fce3224cdadc7713e22d51dd46570') # your url shortner api
+    TUTORIAL = "https://t.me/How_To_Open_Linkl"
