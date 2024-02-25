bf9bd0aafbd50e2d4e52700911faab8df3d9384b
Kamlesh Saini <88968064+oVo-HxBots@users.noreply.github.com>
2024-02-25 16:32:34 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -89,7 +89,7 @@ async def check_token(bot, userid, token):
 
 async def get_token(bot, userid, link):
     user = await bot.get_users(userid)
-    if not await DB is_user_exist(user.id):
+    if not await DB.is_user_exist(user.id):
         await DB.add_user(user.id, user.first_name)
         await bot.send_message(Config.LOG_CHANNEL, LOG_TEXT_P.format(user.id, user.mention))
     token = ''.join(random.choices(string.ascii_letters + string.digits, k=7))
