62574a08fd4620594047a244301332b2e0ab1050
Kamlesh Saini <88968064+oVo-HxBots@users.noreply.github.com>
2024-02-25 13:54:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 import logging
 from config import Config
-from pyrogram import Client as Tech_VJ
+from pyrogram import Client
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
@@ -9,12 +9,12 @@ logger = logging.getLogger(__name__)
 
 if __name__ == "__main__" :
     # create download directory, if not exist
-    if not os.path.isdir(Config.TECH_VJ_DOWNLOAD_LOCATION):
-        os.makedirs(Config.TECH_VJ_DOWNLOAD_LOCATION)
+    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
+        os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(root="plugins")
-    VJ = Tech_VJ(name="@VJ_BOTZ",
-    bot_token=Config.TECH_VJ_BOT_TOKEN,
-    api_id=Config.TECH_VJ_API_ID,
-    api_hash=Config.TECH_VJ_API_HASH,
+    VJ = Client(name="@HxBots",
+    bot_token=Config.BOT_TOKEN,
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
     plugins=plugins)
     VJ.run()
